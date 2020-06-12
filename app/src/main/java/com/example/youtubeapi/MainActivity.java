757cf7a056b3732ddb2aac5a0ede3612cd9fdfa8
Kartik Luthra <luthrakartik@yahoo.com>
2020-06-13 03:03:50 +0530

    Making changes in ScrollView Both Vertically and Horizontally

--- app/src/main/java/com/example/youtubeapi/MainActivity.java
+++ app/src/main/java/com/example/youtubeapi/MainActivity.java
@@ -28,32 +28,138 @@ public class MainActivity extends AppCompatActivity {
 
     private static String GOOGLE_YOUTUBE_API_KEY="AIzaSyDt0oGkMmFY0yGHaD0SsieK3-EQCJhorTE";
     private static String CHANNEL_ID="UCLA_DiR1FfKNvjuUpBHmylQ";
-    private static String CHANNEL_GET_URL="https://www.googleapis.com/youtube/v3/activities?part=snippet%2CcontentDetails&channelId=UCLA_DiR1FfKNvjuUpBHmylQ&maxResults=50&key=AIzaSyDt0oGkMmFY0yGHaD0SsieK3-EQCJhorTE";
+    private static String CHANNEL_GET_URL_NASA="https://www.googleapis.com/youtube/v3/activities?part=snippet%2CcontentDetails&channelId=UCLA_DiR1FfKNvjuUpBHmylQ&maxResults=50&key=AIzaSyDt0oGkMmFY0yGHaD0SsieK3-EQCJhorTE";
+    private static String CHANNEL_GET_URL_SPACEX="https://www.googleapis.com/youtube/v3/activities?part=snippet%2CcontentDetails&channelId=UCtI0Hodo5o5dUb67FeUjDeA&maxResults=10&key=AIzaSyDt0oGkMmFY0yGHaD0SsieK3-EQCJhorTE";
+    private static String CHANNEL_GET_URL_ESA="https://www.googleapis.com/youtube/v3/activities?part=snippet%2CcontentDetails&channelId=UCIBaDdAbGlFDeS33shmlD0A&maxResults=10&key=AIzaSyDt0oGkMmFY0yGHaD0SsieK3-EQCJhorTE";
 
-    private RecyclerView mlist_videos;
+    private RecyclerView mlist_videos,mlist_videos_spacex,mlist_videos_esa;
     private VideoPostAdapter videoPostAdapter=null;
+    private VideoPostAdapterSpaceX videoPostAdapterSpaceX=null;
+    private VideoPostAdapterESA videoPostAdapterESA=null;
     private ArrayList<YoutubeDataModel> mListData=new ArrayList<>();
+    private ArrayList<YoutubeDataModel> mListDataSPACEX=new ArrayList<>();
+    private ArrayList<YoutubeDataModel> mListDataESA=new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mlist_videos=findViewById(R.id.mlist_videos);
+        mlist_videos_spacex=findViewById(R.id.mlist_videos_spacex);
+        mlist_videos_esa=findViewById(R.id.mlist_videos_esa);
         initList(mListData);
+        initListSPACEX(mListDataSPACEX);
+        initListESA(mListDataESA);
 
         //requestQueue= Volley.newRequestQueue(MainActivity.this);
         requestQueue=VoleySingleton.getInstance(this).getRequestQueue();
-        sendApiRequest();
+        sendApiRequestNASA();
+        sendApiRequestSPACEX();
+        sendApiRequestESA();
     }
 
-    private void sendApiRequest() {
-        JsonObjectRequest request=new JsonObjectRequest(Request.Method.GET, CHANNEL_GET_URL, null, new Response.Listener<JSONObject>() {
+    private void sendApiRequestESA() {
+        JsonObjectRequest request=new JsonObjectRequest(Request.Method.GET, CHANNEL_GET_URL_ESA, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try{
+
+                    JSONArray array=response.getJSONArray("items");
+                    for(int i=0;i<array.length();i+=2)
+                    {
+                        JSONObject jsonObject=array.getJSONObject(i);
+                        String title=jsonObject.getJSONObject("snippet").getString("title");
+                        String description=jsonObject.getJSONObject("snippet").getString("description");
+                        String publishedAt=jsonObject.getJSONObject("snippet").getString("publishedAt");
+                        String thumbnail=jsonObject.getJSONObject("snippet").getJSONObject("thumbnails").getJSONObject("medium").getString("url");
+                        YoutubeDataModel youtubeDataModel=new YoutubeDataModel();
+                        youtubeDataModel.setMdescription(description);
+                        youtubeDataModel.setMpublishedAt(publishedAt);
+                        youtubeDataModel.setMtitle(title);
+                        youtubeDataModel.setThumbnail(thumbnail);
+                        mListDataESA.add(youtubeDataModel);
+                        Log.d("List",title);
+                    }
+                    videoPostAdapterESA.notifyDataSetChanged();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        Log.d("in sendApiRequest","Here!!!!");
+        request.setRetryPolicy(new DefaultRetryPolicy(10000, 1, 1.0f));
+
+        requestQueue.add(request);
+
+    }
+
+    private void initListESA(ArrayList<YoutubeDataModel> mListDataESA) {
+        mlist_videos_esa.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false));
+        videoPostAdapterESA=new VideoPostAdapterESA(this, mListDataESA);
+        mlist_videos_esa.setAdapter(videoPostAdapterESA);
+    }
+
+    private void initListSPACEX(ArrayList<YoutubeDataModel> mListDataSPACEX) {
+        mlist_videos_spacex.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false));
+        videoPostAdapterSpaceX=new VideoPostAdapterSpaceX(this, mListDataSPACEX);
+        mlist_videos_spacex.setAdapter(videoPostAdapterSpaceX);
+    }
+
+    private void sendApiRequestSPACEX() {
+        JsonObjectRequest request=new JsonObjectRequest(Request.Method.GET, CHANNEL_GET_URL_SPACEX, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
                 try{
 
                     JSONArray array=response.getJSONArray("items");
                     for(int i=0;i<array.length();i++)
+                    {
+                        JSONObject jsonObject=array.getJSONObject(i);
+                        String title=jsonObject.getJSONObject("snippet").getString("title");
+                        String description=jsonObject.getJSONObject("snippet").getString("description");
+                        String publishedAt=jsonObject.getJSONObject("snippet").getString("publishedAt");
+                        String thumbnail=jsonObject.getJSONObject("snippet").getJSONObject("thumbnails").getJSONObject("medium").getString("url");
+                        YoutubeDataModel youtubeDataModel=new YoutubeDataModel();
+                        youtubeDataModel.setMdescription(description);
+                        youtubeDataModel.setMpublishedAt(publishedAt);
+                        youtubeDataModel.setMtitle(title);
+                        youtubeDataModel.setThumbnail(thumbnail);
+                        mListDataSPACEX.add(youtubeDataModel);
+                        Log.d("List",title);
+                    }
+                    videoPostAdapterSpaceX.notifyDataSetChanged();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        Log.d("in sendApiRequest","Here!!!!");
+        request.setRetryPolicy(new DefaultRetryPolicy(10000, 1, 1.0f));
+
+        requestQueue.add(request);
+
+    }
+
+    private void sendApiRequestNASA() {
+        JsonObjectRequest request=new JsonObjectRequest(Request.Method.GET, CHANNEL_GET_URL_NASA, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try{
+
+                    JSONArray array=response.getJSONArray("items");
+                    for(int i=0;i<array.length();i+=4)
                     {
                         JSONObject jsonObject=array.getJSONObject(i);
                         String title=jsonObject.getJSONObject("snippet").getString("title");
---
+++ app/src/main/java/com/example/youtubeapi/VideoPostAdapterSpaceX.java
@@ -0,0 +1,72 @@
+package com.example.youtubeapi;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class VideoPostAdapterSpaceX extends RecyclerView.Adapter<VideoPostAdapterSpaceX.YoutubePostHolderSpaceX> {
+
+    private ArrayList<YoutubeDataModel> dataSet;
+    private Context context=null;
+
+    public VideoPostAdapterSpaceX(Context context,ArrayList<YoutubeDataModel> dataSet) {
+        this.dataSet = dataSet;
+        this.context=context;
+    }
+
+    @NonNull
+    @Override
+    public YoutubePostHolderSpaceX onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.youtube_post_layout_spacex,parent,false);
+        YoutubePostHolderSpaceX postHolder=new YoutubePostHolderSpaceX(view);
+        return postHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull YoutubePostHolderSpaceX holder, int position) {
+        TextView textViewTitle=holder.textViewTitle;
+        TextView textViewDes=holder.textViewDes;
+        TextView textViewDate=holder.textViewDate;
+        ImageView imageViewThumb=holder.imageViewThumb;
+
+        YoutubeDataModel dataModel=dataSet.get(position);
+        textViewTitle.setText(dataModel.getMtitle());
+        textViewDes.setText(dataModel.getMdescription());
+        textViewDate.setText(dataModel.getMpublishedAt());
+
+        Picasso.with(context).load(dataModel.getThumbnail()).into(imageViewThumb);
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataSet.size();
+    }
+
+    public static class YoutubePostHolderSpaceX extends RecyclerView.ViewHolder{
+
+        TextView textViewTitle,textViewDes,textViewDate;
+        ImageView imageViewThumb;
+
+        public YoutubePostHolderSpaceX(View itemView)
+        {
+            super(itemView);
+            this.textViewTitle=itemView.findViewById(R.id.textViewTitle);
+            this.textViewDes=itemView.findViewById(R.id.textViewDes);
+            this.textViewDate=itemView.findViewById(R.id.textViewDate);
+            this.imageViewThumb=itemView.findViewById(R.id.imageViewThumb);
+        }
+
+
+    }
+}
